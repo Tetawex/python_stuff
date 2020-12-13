f4287f530d5e2933e006ba2a891fb5fcfc53fc17
Tetawex <tetawex@gmail.com>
2020-12-13 18:40:45 +0300

    Exploratory stuff

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ SPEED = 'SPEED'
 data_frame = pd.read_csv("data.csv")
 
 data_frame.describe()
+df['reviews_per_month',
+   'number_of_reviews',
+   'price',
+   'accommodates',
+   'beds',
+   'minimum_nights'
+].append(bool_map(df['host_has_profile_pic'])).append(bool_map(df['host_identity_verified'])).append(
+    bool_map(df['instant_bookable']))
 
 # clustering = KMeans(n_clusters=6).fit(data_frame[[SPEED]])
 # labels = clustering.labels_
