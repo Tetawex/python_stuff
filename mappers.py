7a5f2b8112c83306d2a361a7eee9cd8055e8f999
Tetawex <tetawex@gmail.com>
2020-12-14 04:10:59 +0300

    fix regression

--- mappers.py
+++ mappers.py
@@ -2,13 +2,13 @@ def map_column(column, strategy):
     if strategy == 'Id':
         return column
     elif strategy == 'Boolean':
-        return column.astype('str').map({'t': 1, 'f': 0}).astype('Int64')
+        return column.astype('str').map({'t': 1, 'f': 0}).astype('Float64')
     elif strategy == 'Percentage':
         return column.str.rstrip('%').astype('Float64').divide(100)
     elif strategy == 'Price':
         return column.str.replace(',', '').str.replace('$', '').astype('Float64')
     elif strategy == 'Integer':
-        return column.astype('Int64')
+        return column.astype('Float64')
     elif strategy == 'Float':
         return column.astype('Float64')
     else:
