72d30a49b8150add9f47adfc0c2200d6a4f22e61
Tetawex <tetawex@gmail.com>
2020-12-21 04:48:16 +0300

    ruin all modeling

--- save.py
+++ save.py
@@ -1,7 +1,15 @@
-import shutil
-import time
-1==2;
-while True:
-    time.sleep(5 * 60) # in seconds
-    shutil.copyfile('campaign.data', 'campaign_backup.data')
+xg_reg = xgb.XGBRegressor(objective ='reg:squarederror',
+                          n_jobs = 8,
+                          tree_method='gpu_hist',
+                          colsample_bytree = 0.9,
+                          learning_rate = 0.01,
+                          max_depth = 7,
+                          min_child_weight = 4,
+                          alpha = 0.4,
+                          gamma = 0,
+                          n_estimators = 1000,
+                          seed=123)
 
+xg_reg.fit(X_train,y_train)
+
+preds = xg_reg.predict(X_test)
